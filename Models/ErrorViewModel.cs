1f62b2f81630954df42f0824079ea4a4c43b8b5f
kmloo <kmloo@OWL>
2026-04-05 14:23:05 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace abqwitchesball.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
